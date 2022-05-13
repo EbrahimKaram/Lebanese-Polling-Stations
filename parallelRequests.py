6570b1ee8a077cf4b145dc9a08ec6836e874f158
EbrahimKaram <ebrahim_karam@hotmail.com>
2022-05-13 19:48:38 -0400

    We now have 362 voters found

--- parallelRequests.py
+++ parallelRequests.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import requests
 
 # potential_IDs = range(52693, 1000000000000)
-potential_IDs = range(218287, 300000)
+potential_IDs = range(228604, 300000)
 url = "https://www.dgcs.gov.lb/PollingStations/GetPollingStationExpat"
 
 
