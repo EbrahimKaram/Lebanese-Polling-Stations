82b1d14193e2ee7de3b67aef78f2d39d89f58c4b
Ebrahim Karam <ebrahim_karam@hotmail.com>
2022-05-14 06:59:51 -0400

    Running on ETC Desktop

--- parallelRequests.py
+++ parallelRequests.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import requests
 
 # potential_IDs = range(52693, 1000000000000)
-potential_IDs = range(1000000, 9000000)
+potential_IDs = range(1016392, 2000000)
 url = "https://www.dgcs.gov.lb/PollingStations/GetPollingStationExpat"
 
 
