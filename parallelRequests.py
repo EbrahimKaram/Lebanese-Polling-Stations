6d6b63d068700a8ec368ffa3a618270d5a3b71da
Ebrahim Karam <ebrahim_karam@hotmail.com>
2022-05-15 21:36:42 -0400

    Added a few more lines

--- parallelRequests.py
+++ parallelRequests.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import requests
 
 # potential_IDs = range(52693, 1000000000000)
-potential_IDs = range(3567037, 10000000)
+potential_IDs = range(5077066, 8000000)
 url = "https://www.dgcs.gov.lb/PollingStations/GetPollingStationExpat"
 
 
